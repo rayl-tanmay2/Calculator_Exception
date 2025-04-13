570aa902c04594fa0ab88eede902f06d1dbbe7e7
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-13 15:52:48 +0530

    Method to perform Subtraction.

--- Subtraction.java
+++ Subtraction.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    // Method to perform subtraction of two numbers
+    public static void performSubtraction(Scanner sc) {
+        try {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+
+            double result = a - b;
+            System.out.println("Result = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in subtraction: " + e.getMessage());
+        }
+    }
+}
 
