5cd81cb256286dcdef5a2c0f0c30d4d6881a42fe
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-13 16:09:00 +0530

    Method to calculate Cube

--- Cube.java
+++ Cube.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Cube {
+    // Method to calculate the cube of a number
+    public static void performCube(Scanner sc) {
+        try {
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            double result = a * a * a;
+            System.out.println("Cube = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in calculating cube: " + e.getMessage());
+        }
+    }
+}
 
