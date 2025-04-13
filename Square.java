7a15a673695a8b59d00cd1f75ac3915dd63b6401
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-04-13 15:58:50 +0530

    Method to calculate Square

--- Square.java
+++ Square.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Square {
+    // Method to calculate the square of a number
+    public static void performSquare(Scanner sc) {
+        try {
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            double result = a * a;
+            System.out.println("Square = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in calculating square: " + e.getMessage());
+        }
+    }
+}
 
